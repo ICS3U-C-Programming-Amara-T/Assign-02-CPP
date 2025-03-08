cdf7eecfe93a4a394e44bdf5964ca30d296b4adf
Amara Tie <amara.tie@stu.ocsb.ca>
2025-03-08 03:34:12 +0000

    areaVolumeSphere.cpp

--- areaVolumeSphere.cpp
+++ areaVolumeSphere.cpp
@@ -1,26 +1,25 @@
 // Copyright (c) 2025 Amara Tie All rights reserved.
-
 //
-
 // Created by: Amara Tie
-
 // Date: March 4, 2025
-
 // Calculating the Surface Area and Volume of a Sphere
 #include <cmath>
 #include <iostream>
 #include <string>
 
-    // Color Text Effects
-    const char GREEN[] = "\033[0;34m";
-    const char BOLD_C[] = "\033[31;36m";
-    const char BOLD_P[] = "\033[1;35m";
+// Color Text Effects
+const char GREEN[] = "\033[0;34m";
+const char BOLD_C[] = "\033[31;36m";
+const char BOLD_P[] = "\033[1;35m";
 
 int main() {
     // declare variables
     float radius,
     volume, surfaceArea;
     std::string units;
+
+    // Greeting
+    std::cout << "Hello! Lets find the surface area and volume of a sphere!\n";
 
     // get the radius from the user
     std::cout << "Enter the radius:";
@@ -32,7 +31,7 @@ int main() {
 
     // calculate the surface area and volume
     surfaceArea = 4 * M_PI * pow(radius, 2);
-    volume = 4 / 3 * M_PI * pow(radius, 3);
+    volume = 1.33333333333 * M_PI * pow(radius, 3);
 
     // display the surface area and volume to the user
     std::cout << " ";
